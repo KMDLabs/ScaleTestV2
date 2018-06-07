2cc48965e6d0748206f1286626c2e70372129819
blackjok3r <blackjok3r@gmail.com>
2018-06-07 22:47:36 +0800

    modified:   buildkomodo.sh

--- buildkomodo.sh
+++ buildkomodo.sh
@@ -7,3 +7,13 @@ git clone https://github.com/libscott/komodo.git -b momo
 cd komodo
 ./zcutil/fetch-params.sh
 ./zcutil/build.sh -j$(nproc)
+cd ~
+mkdir .komodo
+cd .komodo
+touch komodo.conf
+echo "rpcuser=user`head -c 32 /dev/urandom | base64`" > komodo.conf
+echo "rpcpassword=password`head -c 32 /dev/urandom | base64`" >> komodo.conf
+echo "daemon=1" >> komodo.conf
+echo "server=1" >> komodo.conf
+echo "txindex=1" >> komodo.conf
+chmod 0600 komodo.conf
